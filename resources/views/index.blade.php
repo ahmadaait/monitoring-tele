014366db55e14804ab37474602095339556f634e
ahmada-it <jamezmada@gmail.com>
2021-11-19 16:03:29 +0700

    change layout index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -98,64 +98,29 @@
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <p>Sensor MQ-7</p>
+                  <p>Device Telemetering 1</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <p>Sensor MQ-131</p>
+                  <p>Device Telemetering 2</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <p>Sensor MQ-136</p>
+                  <p>Weather Forecast</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <p>Sensor NH3</p>
+                  <p>User</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor NO2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor Dust</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor DHT-22</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor BME-280</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor Rain Gauge</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor Anemometer</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <p>Sensor Wind Direction</p>
-                </a>
-              </li>
-              <!-- <li class="nav-item">
                 <a href="#" class="nav-link active">
                   <p>Logout</p>
                 </a>
-              </li> -->
+              </li>
             </ul>
           </li>
         </ul>
@@ -182,16 +147,125 @@
     </section>
 
     <!-- Main content -->
-
+          <!-- <div class="col-12 col-sm-6 col-md-3">
+          </div> -->
           <div class="col-12">
+            <div class="card">
+              <div class="card-header bg-primary">
+              <h3>Data Sensor Telemetering</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body container-fluid">
+                <div class="row">
+                  <a href= "tele1" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor MQ-7</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor MQ-131</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor MQ-136</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor NH3</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor NO2</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor Dust</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor DHT-22</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor Rain Gauge</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor Anemometer</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                  <a href="#" class="btn col-12 col-sm-6 col-md-3 card-link active">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Sensor Wind Direction</span>
+                        <span class="info-box-number"></span>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
             <div class="card">
               <div class="card-header bg-primary">
               <h3>Tentang kami</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-              <p>Sistem Informasi Telemetering Tirtarona</p>
+              <h3>Sistem Informasi Telemetering Tirtarona</h3>
+              <br>
+              <p>Kampung tirta rona adalah kampung edukasi yang dibuat oleh masyarakat rw 7 tlogomas.
+                Wisata edukasi kampung lingkungan tirta rona ini memiliki lahan IPAL komunal yang sering dikunjungi masyarakat 
+                dari berbagai kepnetingan dan wilayah sampai skala internasional. IPAL komunal ini dikelola oleh 
+                Yayasan IPLt Tirta Rona Tologomas Tirta Rona yang diketuai oleh Drs. Agus Gunarto, Mm. Kondisi saat ini, 
+                taman wisata lingkungan tirta rona telah dilengkapi oleh beberapa sarana yang dapat mendukung kegiatan wisata edukasi kampung iklim.</p>
               </div>
+              <div class="card-footer bg-gradient-primary"></div>
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
