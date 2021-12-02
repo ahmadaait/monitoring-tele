449abc1bdb7709779422e0c6d24c1b90ee15a10e
ahmada-it <jamezmada@gmail.com>
2021-12-02 10:21:09 +0700

    save data to database simultaneously

--- resources/views/pages/tele1/index.blade.php
+++ resources/views/pages/tele1/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- <meta http-equiv="refresh" content="20"> -->
+  <meta http-equiv="refresh" content="60">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sistem Informasi Monitoring Cuaca Tirtarona</title>
