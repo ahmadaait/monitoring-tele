b4096e536596f18902d9d52c56cd602265eea622
ahmada-it <jamezmada@gmail.com>
2021-12-10 15:51:39 +0700

    Update iklim1.blade.php

--- resources/views/client_pages/Templating/iklim1.blade.php
+++ resources/views/client_pages/Templating/iklim1.blade.php
@@ -18,7 +18,7 @@
                               <div class="col-20">
                                 <div class="card">
                                   <div class="card-header">
-                                    <h5 class="card-title">Ramalan Cuacas</h5>
+                                    <h5 class="card-title">Ramalan Cuaca</h5>
                                   </div>
                                   <div class="card-body">
                                     <div id="openweathermap-widget-5"></div>
