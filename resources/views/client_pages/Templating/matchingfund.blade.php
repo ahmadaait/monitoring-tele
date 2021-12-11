8f6995e8d8862850f1065e040db5fd36123b2c94
Dwi Ahmad Dzulhijjah <68468571+dwiahmaddzul@users.noreply.github.com>
2021-12-11 15:13:01 +0700

    menambahkan bagian

--- resources/views/client_pages/Templating/matchingfund.blade.php
+++ resources/views/client_pages/Templating/matchingfund.blade.php
@@ -46,5 +46,111 @@
         </div>
     </section>
     <!-- ***** Features Item End ***** -->
-    <!-- ***** Features Item End ***** -->
-@endsection
+
+    <!-- ***** Kegiatan ***** -->
+    <section class="section" id="Kegiatan">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Kegiatan Kami</h2>
+                        <img src="{{asset('client_side/assets/')}}/images/line-dec.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="tabs">
+              <div class="col-lg-4">
+                <ul>
+                  <li><a href='#tabs-1'>Kegiatan Inti</a></li>
+                  <li><a href='#tabs-2'>Kegiatan Tiap Minggu</a></li>
+                  <li><a href='#tabs-3'>Kegiatan Tiap Bulan</a></li>
+                  <li><a href='#tabs-4'>Kegiatan Tiap Tahun</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-8">
+                <section class='tabs-content'>
+                  <article id='tabs-1'>
+                    
+                    <img src="{{asset('client_side/assets/')}}/images/PWisma.jpeg" alt="Second Training">
+                    <h4>Dasa Wisma XI</h4>
+                    <p>Kegiatan untuk pembenahan admiistrasi dari sumber dana kas dawis dan kas PKK</p>
+                    <br> <br>
+                    <img src="{{asset('client_side/assets/')}}/images/PGotongRoyong.jpeg" alt="Second Training">
+                    <h4>Gotong Royong</h4>
+                    <p>Kegiatan untuk membersihkan sekitar kampung Tirtarona</p>
+                    <br> <br>
+                    <img src="{{asset('client_side/assets/')}}/images/PMusholla.jpeg" alt="Second Training">
+                    <h4>Bersih-bersih Musholla</h4>
+                    <p>Kegiatan ini dilaksanakan untuk membersihkan dan merapikan barang-barang yang ada di musholla.</p>
+                    <br> <br>
+
+                    <img src="{{asset('client_side/assets/')}}/images/PJalanRaya.jpeg" alt="Third Class">
+                    <h4>Perbaikan Jalan Raya</h4>
+                    <p>Kegiatan untuk memperbaiki jalan raya di sekitar kampung Tirtarona</p>
+                    <br><br>
+                    <img src="{{asset('client_side/assets/')}}/images/PMCK.jpeg" alt="Third Class">
+                    <h4>MCK</h4>
+                    <p>Kegiatan untuk membenahi dan memperbaiki fasilitas MCK</p>
+                   
+                    <br><br>
+                    
+                    <img src="{{asset('client_side/assets/')}}/images/PHUTRI.jpeg" alt="Fourth Training">
+                    <h4>Peringatan HUT RI</h4>
+                    <p>Kegiatan ini dilaksanakan pada pekan 17 Agustus tiap tahun untuk memperingati HUT RI dengan rangkaian acara upacara atau lomba-lomba</p>
+                    <br><br>
+                    <img src="{{asset('client_side/assets/')}}/images/PBiopori.png" alt="Fourth Training">
+                    <h4>Bipori</h4>
+                    <p>Kegiatan ini dilaksanakan untuk merawat atau pemasangan bipori dari sumber dana swadaya</p>
+                    <br><br>
+                    <img src="{{asset('client_side/assets/')}}/images/PCat.jpeg" alt="Fourth Training">
+                    <h4>Pengecatan</h4>
+                    <p>Kegiatan ini dilaksanakan untuk mengecat berem dari sumber dana swadaya</p>
+                    <br><br>
+
+                  </article>
+
+                  <article id='tabs-2'>
+                    <img src="{{asset('client_side/assets/')}}/images/PWisma.jpeg" alt="Second Training">
+                    <h4>Desa Wisma XI</h4>
+                    <p>Kegiatan untuk pembenahan admiistrasi dari sumber dana kas dawis dan kas PKK</p>
+                    <br> <br>
+                    <img src="{{asset('client_side/assets/')}}/images/PGotongRoyong.jpeg" alt="Second Training">
+                    <h4>Gotong Royong</h4>
+                    <p>Kegiatan untuk membersihkan sekitar kampung Tirtarona</p>
+                    <br> <br>
+                    <img src="{{asset('client_side/assets/')}}/images/PMusholla.jpeg" alt="Second Training">
+                    <h4>Bersih-bersih Musholla</h4>
+                    <p>Kegiatan ini dilaksanakan untuk membersihkan dan merapikan barang-barang yang ada di musholla.</p>
+                    <br> <br>
+                  </article>
+                  <article id='tabs-3'>
+                    <img src="{{asset('client_side/assets/')}}/images/PJalanRaya.jpeg" alt="Third Class">
+                    <h4>Perbaikan Jalan Raya</h4>
+                    <p>Kegiatan untuk memperbaiki jalan raya di sekitar kampung Tirtarona</p>
+                    <br><br>
+                    <img src="{{asset('client_side/assets/')}}/images/PMCK.jpeg" alt="Third Class">
+                    <h4>MCK</h4>
+                    <p>Kegiatan untuk membenahi dan memperbaiki fasilitas MCK</p>
+                   
+                    <br><br>
+                  </article>
+                  <article id='tabs-4'>
+                    <img src="{{asset('client_side/assets/')}}/images/PHUTRI.jpeg" alt="Fourth Training">
+                    <h4>Peringatan HUT RI</h4>
+                    <p>Kegiatan ini dilaksanakan pada pekan 17 Agustus tiap tahun untuk memperingati HUT RI dengan rangkaian acara upacara atau lomba-lomba</p>
+                    <br><br>
+                    <img src="{{asset('client_side/assets/')}}/images/PBiopori.png" alt="Fourth Training">
+                    <h4>Biopori</h4>
+                    <p>Kegiatan ini dilaksanakan untuk merawat atau pemasangan bipori dari sumber dana swadaya</p>
+                    <br><br>
+                    <img src="{{asset('client_side/assets/')}}/images/PCat.jpeg" alt="Fourth Training">
+                    <h4>Pengecatan</h4>
+                    <p>Kegiatan ini dilaksanakan untuk mengecat berem dari sumber dana swadaya</p>
+                </section>
+              </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Our Classes End ***** -->
+
+    @endsection
