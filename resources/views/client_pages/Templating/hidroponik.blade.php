7011e867b613be3be1b4e934cba9c6b66320d9cc
Dwi Ahmad Dzulhijjah <68468571+dwiahmaddzul@users.noreply.github.com>
2021-12-11 19:15:16 +0700

    artikel

--- resources/views/client_pages/Templating/hidroponik.blade.php
+++ resources/views/client_pages/Templating/hidroponik.blade.php
@@ -10,39 +10,91 @@
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-heading">
                         <h2>Hidroponik</h2>
-                        <img src="assets/images/line-dec.png" alt="waves">
+                        <img src="{{asset('client_side/assets/')}}/images/line-dec.png" alt="waves">
                         <br>
                         
                         <body>
-                            <p style="text-align:justify">
-                            Perkembangan teknologi di bidang pertanian begitu pesat, sehingga mereka yang tertinggal dalam memanfaatkan kemajuan teknologi tidak akan memperoleh keuntungan yang maksimal dari kegiatan usaha yang dilakukannya. Salah satu perkembangan teknologi  budidaya pertanian yang layak disebarluaskan adalah teknologi hidroponik. Hal ini disebabkan oleh semakin langkanya sumberdaya lahan, terutama akibat perkembangan sektor industri dan jasa. Sehingga, kegiatan usaha pertanian konvensional semakin tidak kompetitif karena tingginya harga lahan. Teknologi budidaya pertanian sistem hidroponik memberikan alternatif bagi para petani yang memiliki lahan sempit atau yang hanya memiliki pekarangan rumah untuk dapat melaksanakan kegiatan usaha yang dapat dijadikan sebagai sumber penghasilan yang memadai.
-                            </p>                            
-                            <p style="text-align:justify">
-                            Hidroponik berasal dari bahasa latin Hydros yang berarti air dan Phonos yang berarti kerja. Arti harfiah dari hidroponik adalah kerja air. Bertanam secara hidroponik kemudian dikenal dengan bertanam tanpa medium tanah (Soilless Cultivation, Soilless Culture). Mulanya, orang bertanam dengan metode hidroponik menggunakan wadah yang berisi air yang telah dicampur dengan pupuk mikro maupun makro. Budidaya Hydroponic biasanya dilaksanakan di dalam rumah kaca (Greenhouse) untuk menjaga supaya pertumbuhan tanaman secara optimal dan benar – benar terlindung dari pengaruh unsur luar seperti hujan, hama penyakit, iklim dan lain–lain.                            <img src="assets/images/TELE.png" alt="waves" width="400" height="500">
-                            </p>
-                            <br>
-                            <p style="text-align:justify">
-                            Beberapa keuntungan dan kelemahan dari hidroponik, yang pertama keuntungan dari hidroponik ialah keberhasilan tanaman untuk tumbuh dan berproduksi lebih terjamin, perawatan lebih praktis dan gangguan hama lebih terkontrol, pemakaian pupuk lebih hemat (efisien), tanaman yang mati lebih mudah diganti dengan tanaman yang baru, tidak membutuhkan banyak tenaga kasar karena metode kerja lebih hemat dan memiliki standarisasi, tanaman dapat tumbuh lebih pesat dan dengan keadaan yang tidak kotor dan rusak, hasil produksi lebih Continue dan lebih tinggi dibanding dengan penanaman ditanah, harga jual hidroponik lebih tinggi dari produk Non-Hydroponic, beberapa jenis tanaman dapat dibudidayakan di luar musim, tidak ada resiko kebanjiran,erosi, kekeringan, atau ketergantungan dengan kondisi alam, dan tanaman hidroponik dapat dilakukan pada lahan atau ruang yang terbatas, misalnya di atap, dapur atau garasi.                            
-                            </p>
-                            <p style="text-align:justify">
-                            Selain itu, kelemahan dari hidroponik ialah investasi awal yang mahal, memerlukan keterampilan khusus untuk menimbang dan meramu bahan kimia serta ketersediaan dan pemeliharaan perangkat hidroponik agak sulit.
-                            </p>
-                            <p style="text-align:justify">
-                            Prinsip dasar hidroponik dibagi menjadi dua yaitu hidroponik substrat dan NFT (Nutrient Film Technique). Kedua bentuk hidroponik tersebut, dapat dibuat tenik–teknik baru yang dapat disesuaikan dengan kondisi keuangan dan ruang yang tersedia. Hidroponik substrat tidak menggunakan air sebagai media, tetapi menggunakan media padat (bukan tanah) yang dapat menyerap atau menyediakan nutrisi, air, dan oksigen serta mendukung akar tanaman seperti halnya fungsi tanah. Sedangkan, NFT (Nutrient Film Technique)  merupakan model budidaya dengan meletakkan akar tanaman pada lapisan air yang dangkal. Air tersebut tersikulasi dan mengandung nutrisi sesuai kebutuhan tanaman. Perakaran dapat berkembang didalam larutan nutrisi, karena disekitar perakaran terdapat selapis larutan nutrisi maka sistem dikenal dengan nama NFT. Kelebihan air akan mengurangi jumlah oksigen, oleh sebab itu lapisan nutrisi dalam system NFT dibuat maksimal tinggi larutan 3 mm, sehingga kebutuhan air (nutrisi) dan oksigen dapat terpenuhi.                           
-                             </p>
-                             <br>
-                             <br>
-                             
-                            <p style="text-align:justify">
-                        Sumber :    
-                        </p>
-                        <p style="text-align:justify">
-                        Masduki Anang. 2017, “Hidroponik Sebagai Sarana Pemanfaatan Lahan Sempit di Dusun Radubelang, Bangunharjo, Sewon, Bantul”. Jurnal Pemberdayaan, Vol.1, No.2, ISSN: 2580-2569.                         
-                    </p>
-                        <p style="text-align:justify">
-                        Roidah Syamsu Ida. 2014. ”Pemanfaatan Lahan dengan Menggunakan Sistem Hidroponik”. Jurnal Universitas Tulunggagung Bonorowo, Vol.1, No.2.                    </p>
-                            
+                        <img src="{{asset('client_side/assets/')}}/images/ArtikelHidro2.png" alt="waves" width="500" height="300">
 
+                            <p style="text-align:justify">
+                        Kampung iklim RW. 07 Tlogomas yang terletak di jalan tirtarona kelurahan tlogomas kecamatan lowokwaru kota malang provinsi jawa timur. Sempitnya lahan yang berada diwilayah perkampungan ini merupakan faktor penghambat dalam minat penduduk dalam bertanam. Yang menjadi dasar penananam media tanam dengan menggunakan hidroponik di tirtarona adalah sempitnya lahan dikarenakan padatnya penduduk.
+</p>
+<img src="{{asset('client_side/assets/')}}/images/ArtikelHidro.png" alt="waves" width="500" height="300">
+<p style="text-align:justify">
+<br>
+Hidroponik adalah budidaya pertanian tanpa menggunakan media tanah, sehingga hanya dijalankan dengan menggunakan air sebagai pengganti tanah. Pertanian dengan menggunakan sistem hidroponik tidak memerlukan lahan yang luas, dapat diberi nutrisi dengan mudah dan efisien, serta tidak menyebabkan polusi lingkungan. Terdapat beberapa keuntungan dari sistem pertanian hidroponik :
+</p><p style="text-align:justify">
+<br>
+
+1. Keberhasilan tanaman untuk tumbuh dan produksi lebih terjamin. 
+</p><p style="text-align:justify">
+
+2. Perawatan yang praktis 
+</p><p style="text-align:justify">
+
+3. Jika tanaman mati, dapat lebih mudah untuk diganti dengan yang baru 
+</p><p style="text-align:justify">
+
+4. Metode kerja akan lebih hemat tanpa memerlukan banyak tenaga kasar 
+</p><p style="text-align:justify">
+
+5. Tanaman lebih pesat tumbuhnya dan bersih 
+</p><p style="text-align:justify">
+
+6. Harga jual hidroponik lebih tinggi dibanding dengan tanaman di tanahTidak membutuhkan tempat atau lahan yang luas 
+</p><p style="text-align:justify">
+
+7. Beberapa jenis tanaman dapat dibudidayakan di luar musim 
+</p><p style="text-align:justify">
+
+8. Tidak ada resiko kebanjiran,erosi, kekeringan, atau ketergantungan dengan kondisi alam 
+</p><p style="text-align:justify">
+
+9. Tanaman hidroponik dapat dilakukan pada lahan atau ruang yang terbatas, misalnya di atap, dapur atau garasi. 
+</p><p style="text-align:justify">
+
+10. Pemakaian pupuk lebih hemat (efisien) 
+</p><p style="text-align:justify">
+
+11. Hasil produksi lebih continue dan lebih tinggi disbanding dengan penanaman ditanah.
+</p><p style="text-align:justify">
+
+Selain memiliki keuntungan penanaman hidroponik yaitu :
+</p><p style="text-align:justify">
+
+1. Memiliki kelemahan yaitu investasi awal yang mahal 
+</p><p style="text-align:justify">
+
+2. Ketersediaan dan pemeliharaan perangkat hidroponik agak sulit 
+</p><p style="text-align:justify">
+
+3. Memerlukan keterampilan khusus untuk menimbang dan meramu bahan kimia.
+</p><p style="text-align:justify">
+
+Tujuan yang ingin dicapai dari kegiatan Pengembangan Pertanian Hidroponik Berbasis Masyarakat, antara lain :
+</p><p style="text-align:justify">
+
+1. Mengedukasi masyarakat, khususnya RW 07 Kelurahan Tlogomas, mengenai penanaman tanaman hidroponik; 
+</p><p style="text-align:justify">
+
+2. Menentukan desain instalasi hidroponik dan jenis tanaman hidroponik yang akan dikembangkan berdasarkan kesesuaian lahan; 
+</p><p style="text-align:justify">
+
+3. Pembuatan instalasi hidroponik sebagai pertanian berbasis masyarakat Rukun Tetangga;
+</p><p style="text-align:justify">
+
+4. Pemanfaatan air buangan untuk pertanian hidroponik;
+</p><p style="text-align:justify">
+
+Meningkatkan pendapatan mitra dari pengolahan hasil pertanian hidroponik yang berkaitan dengan kegiatan kewirausahaan
+</p>
+<img src="{{asset('client_side/assets/')}}/images/ArtikelHidro3.png" alt="waves" width="250" height="500">
+
+<p style="text-align:justify">
+Hasil dari kegiatan ini adalah terciptanya instalasi hidroponik untuk masyarakat RW 07 Kelurahan Tlogomas berupa 14 paket instalasi hidroponik, seperti pada perencanaan awal kegiatan. Berdasarkan hal tersebut dapat dikatakan bahwa status luaran kegiatan ini telah tercapai, dengan bukti ketercapaian yang ditunjukkan pada gambar berikut.
+</p>
+
+ 
                         </body>
                 
           
